388cdd8210030b012202f8a954d808040d3c23b0
Angelozam17 <angelozam17@gmail.com>
2019-01-18 10:43:01 -0500

    Como yo lo haría 2

--- JS/game.js
+++ JS/game.js
@@ -1,4 +1,64 @@
-let numeroAletorio=size=>{return Math.floor(Math.random()*size);};let obtenerHipotenusa=(event,tsr)=>{let diffX=event.offsetX-tsr.Tesorox;let diffY=event.offsetY-tsr.Tesoroy;return Math.sqrt((diffX*diffX)+(diffY*diffY));};let pista=distancia=>{if(distancia<40){return"Bolling Hot"}else if(distancia<100){return"Hot"}else if(distancia<160){return"Warm"}else if(distancia<220){return"Cold"}else if(distancia<280){return"Really Cold"}else{return"Freezing"}}
-let clicks=0;const ancho=400;const alto=400;const $mapa=document.getElementById('mapa');const $pista_text=document.getElementById('pista');let tesoro={Tesorox:numeroAletorio(ancho),Tesoroy:numeroAletorio(alto),}
-$mapa.addEventListener('click',event=>{clicks++
-let distancia=obtenerHipotenusa(event,tesoro);let distanciaHint=pista(distancia);$pista_text.innerHTML=distanciaHint;if(distancia<20){alert("Has encontrado el tesoro en "+clicks+" clicks");location.reload();}});
+//FUNCIONES
+let numeroAletorio = function(size){
+	return Math.floor(Math.random() * size);
+};
+
+let distanciaDelTesoro = function(event, tsr){
+	let diffX = event.offsetX - tsr.x;
+	let diffY = event.offsetY - tsr.y;
+	return Math.sqrt((diffX*diffX) + (diffY*diffY));
+};
+
+let pistas = function (distancia) {
+	if(distancia < 40){
+		return '¡Demasiado cerca!'
+	}else if (distancia < 80) {
+		return 'Muy cerca'
+	}else if (distancia < 120) {
+		return 'Cerca'
+	}else if (distancia < 160) {
+		return 'Un poco lejos'
+	}else if (distancia < 200) {
+		return 'Lejos'
+	}else if (distancia < 240) {
+		return 'Muy lejos'
+	}else if (distancia < 280) {
+		return 'Demasiado Lejos'
+	}else{
+		return '¡Demasiado Lejos!'
+	};
+};
+let encontrarTesoro = function(event, tsr) {
+	clicks++
+	let distancia = distanciaDelTesoro(event, coordenadasTesoro);
+	$pista_text.innerHTML = pistas(distancia);
+
+	if (distancia < 20) {
+		alert(`¡Haz encontrado el tesoro en ${clicks} clicks!`);
+		location.reload();
+	};
+};
+
+
+//VARIABLES
+const ancho = 624;
+const largo = 400;
+
+let coordenadasTesoro = {
+	x: numeroAletorio(ancho),
+	y: numeroAletorio(largo),
+};
+
+const $mapa = document.getElementById('mapa');
+const $pista_text = document.getElementById('pista');
+let clicks = 0;
+
+//EVENTOS
+$mapa.addEventListener('click', encontrarTesoro);
+
+
+
+
+
+
+
